55f8533fb2ae6cf50fd0e36cfa9c433cfe581dca
Hugo CHEREAU <hchereau@e3r10p13.clusters.42paris.fr>
2023-05-16 22:29:49 +0200

    vertex

--- srcs/utils_matrice_vertex.c
+++ srcs/utils_matrice_vertex.c
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   create_matrice_char.c                              :+:      :+:    :+:   */
+/*   utils_matrice_vertex.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hchereau <hchereau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:02:43 by hchereau          #+#    #+#             */
-/*   Updated: 2023/05/03 16:23:02 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/05/16 21:45:23 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "fdf.h"
-
-//void	create_matrix_char(t_matrice *matrice, int fd)
-//{
-//	size_t	index_col;
-
-//	index_col = 0;
-//	while(index_col < matrice->y)
-//	{
-
-//		++index_col
-//	}
-//}
-
-//-------------------
-
-//char ***content;
-
-//content = read_file(file_path);
-//if (is_valid_map(content) == true)
-//{
-//	map = get_map(content);
-//	print_map(map);
-//}
