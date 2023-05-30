8e84e4d7cf5b780d106654b28de7f279c957ebe0
Hugo CHEREAU <hchereau@e2r12p18.clusters.42paris.fr>
2023-05-30 15:19:06 +0200

    finishparcing

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hchereau <hchereau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 13:17:43 by hchereau          #+#    #+#             */
-/*   Updated: 2023/05/30 13:41:40 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/05/30 15:18:07 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,13 +67,38 @@ static	void	vertex_matrix(size_t nb_line, char ***matrix_char)
 	print_matrix(matrix, nb_line, count_point_on_line(matrix_char));
 }
 
-int	main(__attribute__((unused))int argc, char **argv)
+static	bool	is_valid_path(int argc, char **argv)
+{
+	int		len_arg;
+	bool	is_valid;
+
+	is_valid = false;
+	if (open(argv[1], O_RDONLY) != -1)
+	{
+		if (argc == 2)
+		{
+			len_arg = ft_strlen(argv[1]);
+			if (ft_strncmp(argv[1] + (len_arg - 4), ".fdf", 4) == 0)
+				is_valid = true;
+		}
+	}
+	return (is_valid);
+}
+
+int	main(int argc, char **argv)
 {
 	size_t		nb_line;
 	char		***matrix_char;
 
-	matrix_char = char_matrix(argv[1], &nb_line);
-	if (matrix_char != NULL && is_valid_matrix(matrix_char, nb_line))
-		vertex_matrix(nb_line, matrix_char);
-	free_matrice_char(matrix_char);
+	if (is_valid_path(argc, argv))
+	{
+		matrix_char = char_matrix(argv[1], &nb_line);
+		if (matrix_char != NULL && is_valid_matrix(matrix_char, nb_line))
+			vertex_matrix(nb_line, matrix_char);
+		else
+			ft_printf("error: invalid map\n");
+		free_matrice_char(matrix_char);
+	}
+	else
+		printf("error: invalid argument\n");
 }
--- srcs/utils_matrice_vertex.c
+++ srcs/utils_matrice_vertex.c
@@ -6,7 +6,7 @@
 /*   By: hchereau <hchereau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:02:43 by hchereau          #+#    #+#             */
-/*   Updated: 2023/05/30 14:00:34 by hchereau         ###   ########.fr       */
+/*   Updated: 2023/05/30 15:12:33 by hchereau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ static void	fill_coord_vertex(t_vertex *vertex, size_t i, size_t index_line,
 	vertex->z = ft_atoi(vertex_point[0]);
 }
 
+static void	fill_color_vertex(t_vertex *vertex, char **vertex_point)
+{
+	if (vertex_point[1] != NULL)
+		vertex->color = base_convert(vertex_point[1], BASE_HEXA);
+	else
+		vertex->color = 0xFFFFFFF;
+}
+
 static void	add_vertex(size_t i, char *s, t_vertex **matrix,
 	size_t index_line)
 {
@@ -27,10 +35,7 @@ static void	add_vertex(size_t i, char *s, t_vertex **matrix,
 
 	vertex_point = ft_split(s, ',');
 	fill_coord_vertex(&matrix[index_line][i], i, index_line, vertex_point);
-	if (vertex_point[1] != NULL)
-		matrix[index_line][i].color = base_convert(vertex_point[1], BASE_HEXA);
-	else
-		matrix[index_line][i].color = 0xFFFFFFF;
+	fill_color_vertex(&matrix[index_line][i], vertex_point);
 }
 
 size_t	count_point_on_line(char ***matrix)
